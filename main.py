a3d1d9d091831feb7d4062669159783d85774949
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-08 11:48:25 +0200

    8.2.23

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from obj import Deck
+
+player1 = Deck()
+player2 = Deck()
+
+player1.shuffle()
+print(player1.all_cards[0])
--- obj.py
+++ obj.py
@@ -1,3 +1,8 @@
+import random 
+
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
 values = {"Two": 2, "Three":3, "Four":4, "Five":5, "Six":6,"Seven":7, "Eight":8, "Nine":9, "Ten":10, 'Jack':11, "Queen":12, "King":13, "Ace":14}
 
@@ -10,4 +15,29 @@ class Card:
     def __str__(self):
         return self.rank + " of " + self.suit
 
-    
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                # Create card
+                card = Card(suit, rank)
+                self.all_cards.append(card)
+        # Shuffel my list 
+ 
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+    def __str__(self):
+        return self.all_cards
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+
+
+
+
+
+
